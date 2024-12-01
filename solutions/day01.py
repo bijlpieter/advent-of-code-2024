505cfa0995e9a989ac572c315af3666004afea09
Pieter Bijl <pieterbijl2@gmail.com>
2024-12-01 12:57:51 +0100

    Day 01

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,6 +1,18 @@
+from collections import Counter
+
+
+def parser(input: str) -> tuple[list[int], list[int]]:
+    rows = input.split("\n")
+    pairs = [tuple(map(int, row.split("   "))) for row in rows]
+    return tuple(zip(*pairs))
+
+
 def part1(input: str) -> int:
-    return NotImplemented
+    col1, col2 = parser(input)
+    return sum(abs(left - right) for left, right in zip(sorted(col1), sorted(col2)))
 
 
 def part2(input: str) -> int:
-    return NotImplemented
+    col1, col2 = parser(input)
+    counter = Counter(col2)
+    return sum(val * counter.get(val, 0) for val in col1)
