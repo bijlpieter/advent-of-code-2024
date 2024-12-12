d2c8d01642eeb43e36d3ff25f68a22a4a1756f32
Pieter Bijl <pieterbijl2@gmail.com>
2024-12-12 11:14:18 +0100

    wip

--- solutions/day08.py
+++ solutions/day08.py
@@ -1,5 +1,66 @@
-def part1(input: str) -> int:
-    return NotImplemented
+from collections import defaultdict
+from collections.abc import Callable, Generator
+from fractions import Fraction
+from itertools import combinations
+
+
+class Roof:
+    def __init__(self, input: str) -> None:
+        rows = input.split("\n")
+        self.shape = len(rows), len(rows[0])
+
+        self.antennas = defaultdict(list)
+        for i, row in enumerate(rows):
+            for j, char in enumerate(row):
+                self.antennas[char].append((i, j))
+        del self.antennas["."]
+
+    def antinodes(
+        self, one: tuple[int, int], two: tuple[int, int]
+    ) -> list[tuple[int, int]]:
+        h, w = self.shape
+        r1, c1 = one
+        r2, c2 = two
+        dr, dc = r2 - r1, c2 - c1
+
+        nodes = [(r1 - dr, c1 - dc), (r2 + dr, c2 + dc)]
+        return [(r, c) for r, c in nodes if 0 <= r < h and 0 <= c < w]
+
+    def yield_while_in_range(
+        self, start: tuple[int, int], delta: tuple[int, int]
+    ) -> Generator[tuple[int, int]]:
+        r, c = start
+        while (r, c):
+            pass
+
+    def resonance(
+        self, one: tuple[int, int], two: tuple[int, int]
+    ) -> list[tuple[int, int]]:
+        h, w = self.shape
+        r1, c1 = one
+        r2, c2 = two
+        dr, dc = Fraction(r2 - r1, c2 - c1).as_integer_ratio()
+
+    def solver(
+        self,
+        antinodes: Callable[[tuple[int, int], tuple[int, int]], list[tuple[int, int]]],
+    ) -> int:
+        return len(
+            {
+                antinode
+                for freq, locations in self.antennas.items()
+                for one, two in combinations(locations, 2)
+                for antinode in antinodes(one, two)
+            }
+        )
+
+
+def parser(input: str) -> Roof:
+    return Roof(input)
+
+
+def part1(roof: Roof) -> int:
+    return roof.solver(roof.antinodes)
 
 
 def part2(input: str) -> int:
